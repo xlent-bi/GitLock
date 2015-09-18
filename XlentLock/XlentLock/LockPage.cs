b95de87d874f502a9bea62246c2d8bf73d4874c9
Emil Apelgren <emilapelgren@outlook.com>
2015-09-18 13:03:09 +0200

    Init
    Init

---
+++ XlentLock/XlentLock/LockPage.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XLabs;
+using XLabs.Forms.Behaviors;
+using XLabs.Forms.Mvvm;
+
+namespace XlentLock
+{
+    class LockPage : BaseView
+    {
+        private Button[] _buttons;
+
+        public Button[] Buttons
+        {
+
+            get
+            {
+                return _buttons ?? (_buttons = new Button[]
+                {
+                    new Button()
+                    {
+                        Text = "1"
+                    },
+                    new Button()
+                    {
+                        Text = "2"
+                    },
+                    new Button()
+                    {
+                        Text = "3"
+                    },
+                    new Button()
+                    {
+                        Text = "4"
+                    },
+                    new Button()
+                    {
+                        Text = "5"
+                    },
+                    new Button()
+                    {
+                        Text = "6"
+                    },
+                    new Button()
+                    {
+                        Text = "7"
+                    },
+                    new Button()
+                    {
+                        Text = "8"
+                    },
+                    new Button()
+                    {
+                        Text = "9"
+                    }
+                });
+            }
+            set {  _buttons = value; }
+        }
+
+
+        public Grid ButtonGridLayout;
+
+        public StackLayout MainStackLayout;
+
+		
+
+
+        public LockPage()
+        {
+            ButtonGridLayout = new Grid()
+            {
+                RowDefinitions = new RowDefinitionCollection() { }
+
+            }
+            
+            
+         
+
+
+        }
+
+    }
+}
